dcd8cde9e393fab2a900d67a41a64547287df15e
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-03-11 15:22:52 +0100

    Create Programme.tsx

---
+++ src/components/Programme.tsx
@@ -0,0 +1,14 @@
+const Programme = () => {
+  return (
+    <section className="w-full mt-16">
+      <div className="max-w-6xl mx-auto flex flex-col items-center">
+        <h2 className="text-greenPrimary w-full md:w-9/12 text-[40px] font-bold text-center">
+          Entre plages et pins maritimes pour une qualité de vie aussi
+          <span className="text-bluePrimary"> épanouissante que pratique</span>
+        </h2>
+      </div>
+    </section>
+  );
+};
+
+export default Programme;
