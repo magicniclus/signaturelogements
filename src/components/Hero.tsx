be6367a4891ef5f6c9ee5ef2e0a531370979ebce
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-03-11 15:13:01 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,9 +2,10 @@ import Button from "./Button";
 
 const Hero = () => {
   return (
-    <section className="w-full bg-[url('/images/vague.png')] bg-no-repeat bg-cover bg-bottom">
-      <div className="max-w-6xl md:px-4 min-h-[667px] flex justify-center items-end mx-auto">
-        <div className="bg-bluePrimary rounded-t-full md:rounded-full p-5 w-auto flex w-[522px] md:h-[522px] max-w-full flex-col items-center text-center relative">
+    <section className="w-full md:bg-[url('/images/vague.png')] bg-no-repeat bg-cover bg-bottom flex flex-col relative">
+      <div className="md:hidden flex h-[650px] bg-[url('/images/vagueBig.png')] bg-no-repeat bg-cover bg-top"></div>
+      <div className="max-w-6xl md:px-4 min-h-[667px] flex justify-center items-end mx-auto w-full md:relative absolute -bottom-[250px] md:-bottom-0">
+        <div className="bg-bluePrimary rounded-t-full md:rounded-full p-5 flex w-full md:w-[522px] md:h-[522px] max-w-full flex-col items-center text-center relative">
           <h1 className="text-[40px] md:text-[55px] text-center text-white leading-[50px] mt-14 md:mt-5">
             Devenez <br /> propriétaire <br />{" "}
             <span className="text-[30px] font-light">sur la</span> <br />{" "}
@@ -23,7 +24,7 @@ const Hero = () => {
             placeholder="Email"
             className="p-3 rounded-full w-[290px] md:w-[342px] rounded-lg mt-3 h-[33px]"
           />
-          <div className="flex mb-5 md:mb-20 items-start mt-3 max-0%]">
+          <div className="flex mb-5 md:mb-20 items-start mt-3 max-w-[70%]">
             <input type="checkbox" className="bg-white mt-0.5 text-[15px]" />
             <p className="text-start text-white text-[12px] ml-1">
               J’accepte de recevoir par email des informations de la part de
