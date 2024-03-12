3a71b1cca1a636b8912814a9e8c6cdf2fffdd5ea
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-03-12 14:41:32 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Avantages from "@/components/Avantages";
 import Charm from "@/components/Charm";
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Nav from "@/components/Nav";
 import Programme from "@/components/Programme";
@@ -18,6 +19,7 @@ export default function Home() {
         <Testimonial />
         <Avantages />
       </main>
+      <Footer />
     </>
   );
 }
