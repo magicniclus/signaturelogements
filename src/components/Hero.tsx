3bd604dc2545dd416c695ff4dc5849e31f747a99
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-03-12 15:42:22 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,44 @@
-import Button from "./Button";
+"use client";
+import { FormEvent, useState } from "react";
+import { writeUserData } from "../firebase/datamanager";
 
 const Hero = () => {
+  const [email, setEmail] = useState("");
+  const [isCheckboxChecked, setIsCheckboxChecked] = useState(false);
+  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsCheckboxChecked(e.target.checked);
+  };
+
+  const [isEmailValid, setIsEmailValid] = useState(false);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Submitting:", { email, isCheckboxChecked });
+    const userId = Date.now().toString();
+    writeUserData(email, userId)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const handleEmailChange = (e: FormEvent<HTMLInputElement>) => {
+    const emailValue = (e.target as HTMLInputElement).value;
+    setEmail(emailValue);
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    setIsEmailValid(emailRegex.test(emailValue));
+  };
+
   return (
     <section className="w-full md:bg-[url('/images/vague.png')] bg-no-repeat bg-cover bg-bottom flex flex-col relative">
       <div className="md:hidden flex h-[1000px] bg-[url('/images/vagueBig.png')] bg-no-repeat bg-cover bg-top"></div>
       <div className="max-w-6xl md:px-4 min-h-[667px] flex justify-center items-end mx-auto w-full md:relative absolute -bottom-0">
-        <div className="bg-bluePrimary rounded-t-full md:rounded-full p-5 flex w-full md:w-[522px] md:h-[522px] max-w-full flex-col items-center text-center relative">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-bluePrimary rounded-t-full md:rounded-full p-5 flex w-full md:w-[522px] md:h-[522px] max-w-full flex-col items-center text-center relative"
+        >
           <h1 className="text-[40px] md:text-[55px] text-center text-white leading-[50px] mt-14 md:mt-5">
             Devenez <br /> propriétaire <br />{" "}
             <span className="text-[30px] font-light">sur la</span> <br />{" "}
@@ -23,25 +56,35 @@ const Hero = () => {
             type="text"
             placeholder="Email"
             className="p-3 rounded-full w-[290px] md:w-[342px] rounded-lg mt-3 h-[33px]"
+            value={email}
+            onChange={handleEmailChange}
           />
           <div className="flex mb-5 md:mb-20 items-start mt-3 max-w-[70%]">
-            <input type="checkbox" className="bg-white mt-0.5 text-[15px]" />
+            <input
+              type="checkbox"
+              className="bg-white mt-0.5 text-[15px]"
+              onChange={handleCheckboxChange}
+            />
             <p className="text-start text-white text-[12px] ml-1">
               J’accepte de recevoir par email des informations de la part de
               Signature Promotion.
             </p>
           </div>
-          <Button
-            value="Je veux être contacté"
-            className="bg-orange text-white mb-10 px-10 w-[290px] md:min-w-[342px] md:px-16 text-[20px] md:hidden flex"
-            href="#"
-          />
-          <Button
-            value="Je veux être contacté"
-            className="bg-orange text-white absolute bottom-[-0px] px-16 text-[30px] md:flex hidden"
-            href="#"
-          />
-        </div>
+          <button
+            type="submit"
+            disabled={!isEmailValid}
+            className="p-5 rounded-full border border-white flex justify-center items-center bg-orange text-white mb-10 px-10 w-[290px] md:min-w-[342px] md:px-16 text-[20px] md:hidden flex"
+          >
+            Je veux être contacté
+          </button>
+          <button
+            type="submit"
+            disabled={!isEmailValid}
+            className="p-5 rounded-full border border-white flex justify-center items-center bg-orange text-white absolute bottom-[-0px] px-16 text-[30px] md:flex hidden"
+          >
+            Je veux être contacté
+          </button>
+        </form>
       </div>
     </section>
   );
