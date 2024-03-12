4cbf273aa8ab34219b9d504239c97a907bb54308
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-03-12 14:41:40 +0100

    Update Programme.tsx

--- src/components/Programme.tsx
+++ src/components/Programme.tsx
@@ -65,7 +65,7 @@ const Programme = () => {
   ];
   return (
     <section className="w-full mt-16">
-      <div className="max-w-6xl mx-auto flex flex-col items-center">
+      <div className="max-w-6xl mx-auto px-7 flex flex-col items-center">
         <h2 className="text-greenPrimary w-full md:w-9/12 text-[40px] font-bold text-center">
           Entre plages et pins maritimes pour une qualité de vie aussi
           <span className="text-bluePrimary"> épanouissante que pratique</span>
