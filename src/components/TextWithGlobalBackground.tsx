949dfa2042b52eebb850b685674a6615e4a47e48
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-03-13 19:10:31 +0100

    Update TextWithGlobalBackground.tsx

--- src/components/TextWithGlobalBackground.tsx
+++ src/components/TextWithGlobalBackground.tsx
@@ -8,7 +8,7 @@ gsap.registerPlugin(ScrollTrigger);
 
 const TextWithGlobalBackground = () => {
   useEffect(() => {
-    gsap.set(".text-with-global-background-container", { y: -100, opacity: 0 });
+    gsap.set(".text-with-global-background-container", { y: -50, opacity: 0 });
 
     const tl = gsap.timeline({
       defaults: { duration: 0.5, ease: "power2.out" },
@@ -21,25 +21,25 @@ const TextWithGlobalBackground = () => {
     });
     tl.fromTo(
       ".text-with-global-background-container",
-      { y: -100, opacity: 0 },
+      { y: -50, opacity: 0 },
       { y: 0, opacity: 1, duration: 1, ease: "power2.out" }
     );
   }, []);
   return (
     <section className="w-full bg-[url('/images/interieur.jpg')] bg-no-repeat bg-cover bg-center mt-16 text-with-global-background overflow-hidden">
       <div className="max-w-7xl w-full mx-auto flex md:flex-row flex-col h-full">
-        <div className="w-full md:w-6/12 h-full bg-bluePrimary py-28 px-16 text-with-global-background-container">
-          <h2 className="text-white w-full text-[40px] font-bold leading-[50px]">
+        <div className="w-full md:w-6/12 h-full bg-bluePrimary py-28 px-16 ">
+          <h2 className="text-white w-full text-[40px] font-bold leading-[50px] text-with-global-background-container">
             Des volumes généreux
             <span className="text-greenPrimary"> et ouverts sur la nature</span>
           </h2>
-          <p className="text-start text-white text-[20px] ml-1 mt-7">
+          <p className="text-start text-white text-[20px] ml-1 mt-7 text-with-global-background-container">
             Des espaces de vie confortables qui proposent une{" "}
             <b>double voire triple orientation</b>, ainsi que de{" "}
             <b>larges ouvertures</b> pour un quotidien baigné de lumière
             naturelle.
           </p>
-          <p className="text-start text-white text-[20px] ml-1 mt-5">
+          <p className="text-start text-white text-[20px] ml-1 mt-5 text-with-global-background-container">
             Ambiance bord de mer ou plus contemporaine, laissez libre court à
             votre imagination dans ces logements conçus pour accueillir{" "}
             <b>tous types d’usages</b> : retour de plage, dressing, salle de
