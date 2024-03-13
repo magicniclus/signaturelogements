47043cb74b4947660a7460f5e202e51415983616
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-03-13 14:17:32 +0100

    Update Testimonial.tsx

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -1,3 +1,11 @@
+"use client";
+
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
+
 import {
   Carousel,
   CarouselContent,
@@ -10,17 +18,36 @@ import { StarIcon } from "@heroicons/react/20/solid";
 import Image from "next/image";
 
 const Testimonial = () => {
+  useEffect(() => {
+    gsap.set(".carousel-content", { y: -100, opacity: 0 });
+
+    const tl = gsap.timeline({
+      defaults: { duration: 0.5, ease: "power2.out" },
+      scrollTrigger: {
+        trigger: ".carousel-contant-container",
+        start: "top 80%",
+        end: "top 50%",
+        // markers: true,
+      },
+    });
+    tl.fromTo(
+      ".carousel-content",
+      { y: -100, opacity: 0 },
+      { y: 0, opacity: 1, duration: 1, ease: "power2.out" }
+    );
+  }, []);
+
   return (
-    <section className="w-full bg-greenPrimary">
-      <div className="max-w-6xl mx-auto py-16 px-4 md:px-0 flex items-center flex-col">
-        <h2 className="text-white w-full text-[40px] font-bold text-center">
+    <section className="w-full bg-greenPrimary carousel-contant-container overflow-hidden">
+      <div className="max-w-7xl mx-auto py-16 px-4 md:px-0 flex items-center flex-col">
+        <h2 className="text-white w-full text-[40px] font-bold text-center carousel-content">
           100%
           <span className="text-bluePrimary">
             {" "}
             de nos clients nous recommandent
           </span>
         </h2>
-        <div className="w-[90%] mt-10">
+        <div className="w-[90%] mt-10 carousel-content">
           <Carousel>
             <CarouselContent>
               <CarouselItem>
@@ -60,7 +87,7 @@ const Testimonial = () => {
                 </div>
               </CarouselItem>
               <CarouselItem>
-                <div className="flex items-center justify-center">
+                <div className="flex items-center justify-center carousel-content">
                   <div className="h-[100px] w-[100px] flex justify-center items-center bg-orange rounded-full">
                     <p className="text-white text-[43px]">M</p>
                   </div>
@@ -136,14 +163,14 @@ const Testimonial = () => {
             <CarouselNext />
           </Carousel>
         </div>
-        <div className="flex mt-7">
+        <div className="flex mt-7 .carousel-content">
           <StarIcon className="w-[24px] h-auto text-yellowPrimary" />
           <StarIcon className="w-[24px] h-auto text-yellowPrimary ml-2" />
           <StarIcon className="w-[24px] h-auto text-yellowPrimary ml-2" />
           <StarIcon className="w-[24px] h-auto text-yellowPrimary ml-2" />
           <StarIcon className="w-[24px] h-auto text-yellowPrimary ml-2" />
         </div>
-        <p className="mt-3 text-bluePrimary text-[16px] text-center">
+        <p className="mt-3 text-bluePrimary text-[16px] text-center .carousel-content">
           Visite cloisons, avis déposé le 23 novembre 2023
         </p>
         <Image
@@ -151,7 +178,7 @@ const Testimonial = () => {
           width={121}
           height={121}
           alt="immodvisor"
-          className="mt-5"
+          className="mt-5 .carousel-content"
         />
       </div>
     </section>
