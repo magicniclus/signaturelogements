5d012392dc3ffcb88529631d4d74dab486cc79e6
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-03-13 14:17:01 +0100

    Update Charm.tsx

--- src/components/Charm.tsx
+++ src/components/Charm.tsx
@@ -1,10 +1,40 @@
+"use client";
+
+import { useEffect } from "react";
 import Button from "./Button";
 
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
 const Charm = () => {
+  useEffect(() => {
+    gsap.set(".charm-text", { y: -100, opacity: 0 });
+
+    const tl = gsap.timeline({
+      defaults: { duration: 0.5, ease: "power2.out" },
+      scrollTrigger: {
+        trigger: ".charm-container",
+        start: "top 80%",
+        end: "top 50%",
+        // markers: true,
+      },
+    });
+    tl.fromTo(
+      ".charm-text",
+      { y: -100, opacity: 0 },
+      { y: 0, opacity: 1, duration: 1, ease: "power2.out" }
+    );
+  }, []);
+
   return (
-    <section className="w-full md:px-7" id="atlantique">
-      <div className="max-w-6xl mx-auto rounded-lg md:mt-20 flex min-h-[526px] overflow-hidden md:flex-row flex-col">
-        <div className="md:w-1/2 w-full py-10 px-8 bg-greenPrimary flex flex-col justify-between">
+    <section
+      className="w-full md:px-7 charm-container overflow-hidden"
+      id="atlantique"
+    >
+      <div className="max-w-7xl mx-auto rounded-lg md:mt-20 flex min-h-[526px] overflow-hidden md:flex-row flex-col">
+        <div className="md:w-1/2 w-full py-10 px-8 bg-greenPrimary flex flex-col justify-between charm-text">
           <h2 className="text-[40px] font-bold text-white">
             Tombez sous le charme des{" "}
             <span className="text-bluePrimary">joyaux de l’atlantique</span>{" "}
