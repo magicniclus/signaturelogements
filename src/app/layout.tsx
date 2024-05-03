704e24c23127844a7cd646dab959ce38abd6b5a6
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-05-03 11:03:36 +0200

    update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <head>
-        <link rel="icon" href="/logos/A-500x500.png" sizes="any" />
+        <link rel="icon" href="/logo.png" sizes="any" />
       </head>
       <body className={larsseit.className}>{children}</body>
       <GoogleTagManager gtmId="GTM-5GNQ636R" />
