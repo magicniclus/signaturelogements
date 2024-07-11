39ff7968748c5f7115f43de44db1e553af8784e9
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-07-11 10:49:10 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Avantages from "@/components/Avantages";
 import Charm from "@/components/Charm";
 import Footer from "@/components/Footer";
@@ -6,8 +8,31 @@ import Nav from "@/components/Nav";
 import Programme from "@/components/Programme";
 import Testimonial from "@/components/Testimonial";
 import TextWithGlobalBackground from "@/components/TextWithGlobalBackground";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const handleSmoothScroll = (event: MouseEvent) => {
+      const target = event.target as HTMLElement;
+      if (target.matches("a[href^='#']")) {
+        event.preventDefault();
+        const id = target.getAttribute("href")?.slice(1);
+        if (id) {
+          const element = document.getElementById(id);
+          if (element) {
+            element.scrollIntoView({ behavior: "smooth" });
+          }
+        }
+      }
+    };
+
+    document.addEventListener("click", handleSmoothScroll);
+
+    return () => {
+      document.removeEventListener("click", handleSmoothScroll);
+    };
+  }, []);
+
   return (
     <>
       <Nav nav={true} />
