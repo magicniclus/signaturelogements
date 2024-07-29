d1a038c2b19360bd59f2f6d1c1e23c4c680a1174
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-07-29 17:33:53 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 
 const Footer = () => {
   return (
-    <footer className="w-full bg-bluePrimary flex flex-col">
+    <footer className="w-full bg-[#1D2749] flex flex-col">
       <div className="max-w-7xl mx-auto w-full py-16 px-7 flex justify-between items-center lg:flex-row flex-col h-full">
         <div className="flex flex-col">
           <Image
@@ -29,7 +29,7 @@ const Footer = () => {
           <Button
             href="#form"
             value="Contactez-moi"
-            className="bg-orange text-white md:h-[83px] w-[287px] md:mt-0 mt-8"
+            className="bg-bluePrimary text-white md:h-[83px] w-[287px] md:mt-0 mt-8"
           />
         </div>
         <div className="flex flex-col md:h-[180px] justify-between md:mt-0 mt-10 items-center">
@@ -39,21 +39,9 @@ const Footer = () => {
           <Button
             href="tel:0556367038"
             value="05 56 36 70 38"
-            className="bg-orange text-white md:h-[83px] w-[287px] md:mt-0 mt-8"
+            className="bg-bluePrimary text-white md:h-[83px] w-[287px] md:mt-0 mt-8"
           />
         </div>
-      </div>
-
-      <div className="max-w-7xl mx-auto w-full py-16 px-7 flex justify-between items-center lg:flex-row flex-col h-full">
-        <p className="text-white text-xs">
-          *Offres commerciales exceptionnelles appliquées sur les prochains
-          contrats de réservation signés entre le 01/07/2024 et le 31/08/2024
-          inclus, dans le cadre de l’achat d’un logement neuf au sein d’un
-          programme réalisé par Signature Promotion. Cette offre est cumulable
-          avec les autres offres Signature Promotion en cours ou à venir sur la
-          période susvisée et pourra éventuellement être prolongée, sous
-          conditions.
-        </p>
       </div>
     </footer>
   );
