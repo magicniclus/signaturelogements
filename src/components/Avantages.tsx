1a1e55fb601a0b6fddb7a65aef19acb21a3f38f5
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-07-29 17:34:10 +0200

    Update Avantages.tsx

--- src/components/Avantages.tsx
+++ src/components/Avantages.tsx
@@ -33,38 +33,38 @@ const Avantages = () => {
     <section className="w-full avantages overflow-hidden" id="engagement">
       <div className="max-w-7xl mx-auto py-16 px-7 flex items-center flex-col avantages-container">
         <Image src="/logos/logoBig.png" width={412} height={100} alt="logo" />
-        <h2 className="text-bluePrimary w-full text-[40px] font-bold text-center w-9/12 mt-10">
+        <h2 className="text-[#1D2749] w-full text-[40px] font-bold text-center w-9/12 mt-10">
           Le sur-mesure{" "}
-          <span className="text-greenPrimary">
+          <span className="text-bluePrimary">
             plutôt que le tout identique pour
           </span>{" "}
           satisfaire nos clients propriétaires
         </h2>
         <div className="w-full flex justify-around items-center mt-16 md:flex-row flex-col">
           <div className="flex flex-col items-center">
-            <div className="p-5 rounded-full bg-bluePrimary flex justify-center items-center h-[100px] w-[100px]">
+            <div className="p-5 rounded-full bg-[#1D2749] flex justify-center items-center h-[100px] w-[100px]">
               <Image src="/icons/hand.png" width={60} height={60} alt="house" />
             </div>
             <div className="mt-3">
-              <h3 className="text-bluePrimary text-center">
+              <h3 className="text-[#1D2749] text-center">
                 Un acteur <br />
                 <b>DE PROXIMITÉ</b>
               </h3>
             </div>
           </div>
           <div className="flex flex-col items-center md:mt-0 mt-7">
-            <div className="p-5 rounded-full bg-bluePrimary flex justify-center items-center h-[100px] w-[100px]">
+            <div className="p-5 rounded-full bg-[#1D2749] flex justify-center items-center h-[100px] w-[100px]">
               <Image src="/icons/hand.png" width={60} height={60} alt="house" />
             </div>
             <div className="mt-3">
-              <h3 className="text-bluePrimary text-center">
+              <h3 className="text-[#1D2749] text-center">
                 Des architectures pour <br />
                 <b>VIVRE HEUREUX</b>
               </h3>
             </div>
           </div>
           <div className="flex flex-col items-center md:mt-0 mt-7">
-            <div className="p-5 rounded-full bg-bluePrimary flex justify-center items-center h-[100px] w-[100px]">
+            <div className="p-5 rounded-full bg-[#1D2749] flex justify-center items-center h-[100px] w-[100px]">
               <Image
                 src="/icons/smiley.png"
                 width={60}
@@ -73,7 +73,7 @@ const Avantages = () => {
               />
             </div>
             <div className="mt-3">
-              <h3 className="text-bluePrimary text-center">
+              <h3 className="text-[#1D2749] text-center">
                 Une construction <br />
                 <b>DURABLE</b>
               </h3>
