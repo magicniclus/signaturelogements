dc0130b25d0f92c93cfc8b04b94534b12edc5da2
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-07-31 10:09:50 +0200

    Update Testimonial.tsx

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -1,24 +1,19 @@
-"use client";
-
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import dynamic from "next/dynamic";
+import Image from "next/image";
 import { useEffect } from "react";
+
+// Charger dynamiquement le widget Immodvisor
+const ImmodvisorWidget = dynamic(() => import("./ImmodvisorWidget"), {
+  ssr: false,
+});
 
 gsap.registerPlugin(ScrollTrigger);
 
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
-
-import { StarIcon } from "@heroicons/react/20/solid";
-import Image from "next/image";
-
 const Testimonial = () => {
   useEffect(() => {
+    // GSAP animations
     gsap.set(".carousel-content", { y: -100, opacity: 0 });
 
     const tl = gsap.timeline({
@@ -27,7 +22,6 @@ const Testimonial = () => {
         trigger: ".carousel-contant-container",
         start: "top 80%",
         end: "top 50%",
-        // markers: true,
       },
     });
     tl.fromTo(
@@ -48,125 +42,9 @@ const Testimonial = () => {
           </span>
         </h2>
         <div className="w-[90%] mt-10 carousel-content">
-          <Carousel>
-            <CarouselContent>
-              <CarouselItem>
-                <div className="flex items-center justify-center">
-                  <div className="h-[100px] w-[100px] flex justify-center items-center bg-[#D66542] rounded-full">
-                    <p className="text-white text-[43px]">M</p>
-                  </div>
-                  <div className="w-9/12 flex flex-col ml-5">
-                    <h3 className="text-[#1D2749] text-[20px] font-bold">
-                      « ACCUEUIL SYMPATHIQUE ET PROFESSIONNEL »
-                    </h3>
-                    <p className="text-[#1D2749] text-[20px]">
-                      Entreprise très sérieuse Nous apprécions d’être tenus
-                      informés régulièrement sur l’avancement des travaux Très
-                      bon accueil à chaque rencontre avec un personnel très
-                      compétent{" "}
-                    </p>
-                  </div>
-                </div>
-              </CarouselItem>
-              <CarouselItem>
-                <div className="flex items-center justify-center">
-                  <div className="h-[100px] w-[100px] flex justify-center items-center bg-[#D66542] rounded-full">
-                    <p className="text-white text-[43px]">L</p>
-                  </div>
-                  <div className="w-9/12 flex flex-col ml-5">
-                    <h3 className="text-[#1D2749] text-[20px] font-bold">
-                      « Professionnel et compétent »
-                    </h3>
-                    <p className="text-[#1D2749] text-[20px]">
-                      Très agréable accompagnement et travail qui nous convient
-                      très bien. Merci.
-                    </p>
-                  </div>
-                </div>
-              </CarouselItem>
-              <CarouselItem>
-                <div className="flex items-center justify-center carousel-content">
-                  <div className="h-[100px] w-[100px] flex justify-center items-center bg-[#D66542] rounded-full">
-                    <p className="text-white text-[43px]">G</p>
-                  </div>
-                  <div className="w-9/12 flex flex-col ml-5">
-                    <h3 className="text-[#1D2749] text-[20px] font-bold">
-                      « Agence très compétente »
-                    </h3>
-                    <p className="text-[#1D2749] text-[20px]">
-                      Bon accueil, personnel sérieux et professionnel, réponse
-                      rapide à nos questions.
-                    </p>
-                  </div>
-                </div>
-              </CarouselItem>
-              <CarouselItem>
-                <div className="flex items-center justify-center">
-                  <div className="h-[100px] w-[100px] flex justify-center items-center bg-[#D66542] rounded-full">
-                    <p className="text-white text-[43px]">I</p>
-                  </div>
-                  <div className="w-9/12 flex flex-col ml-5">
-                    <h3 className="text-[#1D2749] text-[20px] font-bold">
-                      « BIEN »
-                    </h3>
-                    <p className="text-[#1D2749] text-[20px]">
-                      Jusqu’à présent tout c’est très bien passé. Efficacité des
-                      différentes personnes, cordialité… Le projet est bien
-                      avancé dans les temps annoncés. L’organisation fonctionne
-                      bien.
-                    </p>
-                  </div>
-                </div>
-              </CarouselItem>
-              <CarouselItem>
-                <div className="flex items-center justify-center">
-                  <div className="h-[100px] w-[100px] flex justify-center items-center bg-[#D66542] rounded-full">
-                    <p className="text-white text-[43px]">M</p>
-                  </div>
-                  <div className="w-9/12 flex flex-col ml-5">
-                    <h3 className="text-[#1D2749] text-[20px] font-bold">
-                      « ACCUEUIL SYMPATHIQUE ET PROFESSIONNEL »
-                    </h3>
-                    <p className="text-[#1D2749] text-[20px]">
-                      Entreprise très sérieuse Nous apprécions d’être tenus
-                      informés régulièrement sur l’avancement des travaux Très
-                      bon accueil à chaque rencontre avec un personnel très
-                      compétent{" "}
-                    </p>
-                  </div>
-                </div>
-              </CarouselItem>
-              <CarouselItem>
-                <div className="flex items-center justify-center">
-                  <div className="h-[100px] w-[100px] flex justify-center items-center bg-[#D66542] rounded-full">
-                    <p className="text-white text-[43px]">A</p>
-                  </div>
-                  <div className="w-9/12 flex flex-col ml-5">
-                    <h3 className="text-[#1D2749] text-[20px] font-bold">
-                      « Professionnel et compétent »
-                    </h3>
-                    <p className="text-[#1D2749] text-[20px]">
-                      Très agréable accompagnement et travail qui nous convient
-                      très bien. Merci.
-                    </p>
-                  </div>
-                </div>
-              </CarouselItem>
-            </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
-          </Carousel>
+          {/* Widget container */}
+          <ImmodvisorWidget />
         </div>
-        <div className="flex mt-7 carousel-content">
-          <StarIcon className="w-[24px] h-auto text-[#FFE042]" />
-          <StarIcon className="w-[24px] h-auto text-[#FFE042] ml-2" />
-          <StarIcon className="w-[24px] h-auto text-[#FFE042] ml-2" />
-          <StarIcon className="w-[24px] h-auto text-[#FFE042] ml-2" />
-          <StarIcon className="w-[24px] h-auto text-[#FFE042] ml-2" />
-        </div>
-        <p className="mt-3 text-[#1D2749] text-[16px] text-center carousel-content">
-          Visite cloisons, avis déposé le 23 novembre 2023
-        </p>
         <Image
           src={"/images/seal.png"}
           width={121}
